30385b7278e46a6c221d7324b606b209724d0169
Md. Touhidur Rahman <touhidurrahmant1@gmail.com>
2024-10-31 20:52:46 +0000

    remove exit handler

--- index.js
+++ index.js
@@ -19,8 +19,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
-
-process.once("SIGKILL", () => {
-  console.log("test");
-  require('fs').rmSync('uploads', { recursive: true });
-});
