294a8349ac2bb1aad12043b19ab77124c321032b
Md. Touhidur Rahman <touhidurrahmant1@gmail.com>
2024-10-31 20:26:24 +0000

    restructure project and implement api

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const cors = require('cors');
+const multer = require("multer");
+const express = require('express');
 
-var app = express();
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+// File Metadata Microservice
+const upload = multer({ dest: "uploads/" });
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const { originalname: name, mimetype: type, size } = req.file;
+  res.json({ name, type, size });
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
